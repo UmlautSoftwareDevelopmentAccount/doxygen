49f8fdbfb3050117b4dfcab70bec0074f81c2264
Krzysztof Ostrowski <insooth@users.noreply.github.com>
2020-09-17 12:56:34 +0200

    Revert "Refactoring: making sqlcode.l reentrant"
    This reverts commit bf1b9f4357f9d32fa4149c8fef25bc362345a9c7.

--- src/sqlcode.h
+++ src/sqlcode.h
@@ -1,10 +1,12 @@
 /******************************************************************************
  *
- * Copyright (C) 1997-2020 by Dimitri van Heesch.
+ * 
+ *
+ * Copyright (C) 1997-2014 by Dimitri van Heesch.
  *
  * Permission to use, copy, modify, and distribute this software and its
- * documentation under the terms of the GNU General Public License is hereby
- * granted. No representations are made about the suitability of this software
+ * documentation under the terms of the GNU General Public License is hereby 
+ * granted. No representations are made about the suitability of this software 
  * for any purpose. It is provided "as is" without express or implied warranty.
  * See the GNU General Public License for more details.
  *
@@ -30,8 +32,6 @@ class Definition;
 class SQLCodeParser : public CodeParserInterface
 {
   public:
-    SQLCodeParser();
-    virtual ~SQLCodeParser();
     void parseCode(CodeOutputInterface &codeOutIntf,
                    const char *scopeName,
                    const QCString &input,
@@ -48,9 +48,6 @@ class SQLCodeParser : public CodeParserInterface
                    bool collectXRefs=TRUE
                   );
     void resetCodeParserState();
-  private:
-    struct Private;
-    std::unique_ptr<Private> p;
 };
 
 
