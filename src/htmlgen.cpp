302c4d00aa63644435443dc818d7486c8812782d
Krzysztof Ostrowski <insooth@users.noreply.github.com>
2020-09-17 12:56:34 +0200

    Revert "Fixed regression causing search.css not to be generated anymore"
    This reverts commit 7bacbd113223407496a6643995eb26f1d0c589c4.

--- src/htmlgen.cpp
+++ src/htmlgen.cpp
@@ -1070,7 +1070,7 @@ void HtmlGenerator::writeSearchData(const char *dname)
     Doxygen::indexList->addImageFile("search/mag_sel.svg");
   }
 
-  QCString searchDirName = dname;
+  QCString searchDirName = QCString(dname)+"/search";
   QFile f(searchDirName+"/search.css");
   if (f.open(IO_WriteOnly))
   {
